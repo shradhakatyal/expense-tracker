924442595c63ba79c49bfdbc00e8f4d3d5ea7d0a
shradhakatyal <skatyal2204@gmail.com>
2020-07-03 23:08:33 +0530

    Updates

---
+++ precache-manifest.2c30ee0e839d9b742b4314ee07edd3a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33279f810f72240375ae3934eff8dc8e",
+    "url": "/shradhakatyal/expense-tracker.git/index.html"
+  },
+  {
+    "revision": "46e7a8b21e3123dcbcc0",
+    "url": "/shradhakatyal/expense-tracker.git/static/css/main.c0ec7200.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/shradhakatyal/expense-tracker.git/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shradhakatyal/expense-tracker.git/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e7a8b21e3123dcbcc0",
+    "url": "/shradhakatyal/expense-tracker.git/static/js/main.0595a670.chunk.js"
+  },
+  {
+    "revision": "998edcdb2ebee13b47df",
+    "url": "/shradhakatyal/expense-tracker.git/static/js/runtime-main.a6ea9f35.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/shradhakatyal/expense-tracker.git/precache-manifest.2c30ee0e839d9b742b4314ee07edd3a9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shradhakatyal/expense-tracker.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
